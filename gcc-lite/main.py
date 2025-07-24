9e771b29f6ed018c3dcabf30f7e8fbd15e32eb23
jday7879 <57715292+Jday7879@users.noreply.github.com>
2025-07-24 10:18:44 +0100

    creating optional entry points for package

--- gcc-lite/main.py
+++ gcc-lite/main.py
@@ -1,14 +1,28 @@
-import subprocess
+from cookiecutter.main import cookiecutter
+
 
 def run_gcc_lite():
     try:
         # Run the gcc-lite command
-        repo_url = "https://github.com/best-practice-and-impact/govcookiecutter-lite.git"
-        subprocess.run(['cookiecutter', repo_url], check=True)
-    except subprocess.CalledProcessError as e:
-        print("An error occurred while running gcc-lite:")
-        print(e.stderr)
+        repo_url = (
+            "https://github.com/best-practice-and-impact/govcookiecutter-lite.git"
+        )
+        cookiecutter(
+            template=repo_url,
+        )
+    except Exception as e:
+        print(f"An error occurred while running gcc-lite: {e}")
+
+
+def gcc_lite_example():
+    try:
+        # Run the gcc-example command
+        # Alternate: Use the cookiecutter Python API directly
+        cookiecutter(template=".", output_dir="./gcc-lite/example", no_input=True)
+    except Exception as e:
+        print(f"An error occurred while running gcc-example: {e}")
 
 
 if __name__ == "__main__":
+    gcc_lite_example()
     run_gcc_lite()
